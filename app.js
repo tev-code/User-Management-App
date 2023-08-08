ee41474186310e2aa7bda790f6838c6234160c1e
Tevin <tevin@trade245.com>
2023-08-08 14:18:59 +0200

    frontend

--- app.js
+++ app.js
@@ -54,7 +54,7 @@ app.get('/users', (req, res) => {
     });
 });
 
-// Get a single user byy ID
+// Get a single user by ID
 app.get('/users/:id', (req, res) => {
     const userId = req.params.id;
         db.collection('users').findOne({ _id: new mongodb.ObjectID(userId) }, (err, user) => {
@@ -103,4 +103,94 @@ app.delete('/users/:id', (req, res) => {
     }
     });
 });
+
+// Implement the frontend logic to interact with the backend server using the Fetch API
+const userForm = document.getElementById('userForm');
+const userList = document.getElementById('userList');
+const nameInput = document.getElementById('name');
+const emailInput = document.getElementById('submitbutton');
+
+const apiUrl = 'http://localhost:3000/users';
+
+// Function to display the list of users
+function displayUsers(users) {
+    userList.innerHTML = '';
+
+    users.forEach(user => {
+    const userDiv = document.createElement('div');
+    userDiv.classList.add('user');
+    userDiv.innerHTML = `
+     <p><strong>Name:</strong> ${user.name}</p>
+     <p><strong>Email:</strong> ${user.email}</p>
+     <button onclick="editUser('${user._id}', '${user.name}', '${user.email}')">Edit</button>
+     <button onclick="deleteUser('${user._id}')">Delete</button>
+    `;
+    userList.appendChild(userDiv);
+    });
+}
+
+// Function to create/update a user
+async function submitUser(event) {
+ event.preventDefault();
+
+ const name = nameInput.value;
+ const email = emailInput.value;
+
+ try {
+    let response;
+
+    if (submitButton.innerText === 'Create User') {
+      response = await fetch(apiUrl, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ name, email }),
+      });
+    } else {
+        const userId = submitButton.dataset.id;
+        response = await fetch(`${apiUrl}/${userId}`, {
+           method: 'PUT',
+           headers: {
+            'Content-Type': 'application/json'
+           },
+           body: JSON.stringify({ name, email}),
+        });
+
+        submitButton.innerText = 'Create User';
+    }
+
+    nameInput.value = '';
+    emailInput.value = '';
+    submitButton.dataset.id = '';
+
+    getUsers();
+ } catch (error) {
+    console.error('Error creating/updating user:', error);
+ }
+}
+
+// Function to delete user
+async function deleteUser(userId) {
+    try {
+        await fetch(`${apiUrl}/${userId}`, { method: 'DELETE'});
+        getUsers();
+    } catch (error) {
+        console.error('Error deleting user:', error);
+    }
+}
+
+// Function to populate the form for editing a user
+function editUser(userId, userName, userEmail) {
+    nameInput.value = userName;
+    emailInput.value = userEmail;
+    submitButton.innerText = 'Update User';
+    submitButton.dataset.id = userId;
+}
+
+// Event listeners
+userForm.addEventListener('submit', submitUser);
+
+// Fetch and display users when the page loads
+getUsers();
 
